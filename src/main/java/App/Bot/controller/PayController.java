f39504d179bdd0b26902b88aebafbb38f50ac46d
Alexandr Rube <alexrube93@gmail.com>
2023-08-17 14:14:39 -0300

    https ok

--- src/main/java/App/Bot/controller/PayController.java
+++ src/main/java/App/Bot/controller/PayController.java
@@ -3,21 +3,19 @@ package App.Bot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
-
 @RestController
 public class PayController {
-    @GetMapping("/notification")
+    @GetMapping("/freekassa_notification")
     public void notification(String notification) {
         System.out.println(notification);
     }
 
-    @GetMapping("/bad")
+    @GetMapping("/freekassa_bad")
     public void bad() {
-        System.out.println("bad");
+        System.out.println("freekassa_bad");
     }
 
-    @GetMapping("/good")
+    @GetMapping("/freekassa_good")
     public void good() {
         System.out.println("good");
     }
